9378cc2b40a12451d9ac65a5484c9387c95787bb
maasam514 <mohammadsam.karimi.1@ens.etsmtl.ca>
2024-03-19 20:10:04 -0400

    calGuest (not complete) probleme avec la base de donnée

---
+++ frontend/calGuest.js
@@ -0,0 +1,61 @@
+let teamArray = new Array();
+
+teamArray[0] = 3;//test
+teamArray[1] = 3;//test
+
+for (let i = 0; i < teamArray.length && teamArray[i] !== undefined; i++) 
+{
+    let team = teamArray[i];
+    let newRow = document.createElement("tr");
+    newRow.id = `${team}`;
+    newRow.innerHTML = `<tr>${team}</tr>`;
+    document.getElementById("teams").appendChild(newRow);
+}
+
+let numMatch, numWin, numLoss, points, numDraw, numRecu, numGoal;
+
+//code d'interaction avec database
+
+
+let teamstable=document.getElementById("teams");
+teamstable.addEventListener("click", function(event)
+{
+    if (event.target.tagName ==="TR") 
+    {
+        let teamId= event.target.id.team;    
+
+        let teamStat = getTeamstats(teamId);
+
+        updateStatRow(teamStat);
+    }
+})
+
+function getTeamstats(teamId) 
+{
+    //interaction avec database
+
+    return{// test
+        "Team": teamName,
+        "Wins": Math.floor(Math.random() * 10),
+        "Losses": Math.floor(Math.random() * 10),
+        "Draws": Math.floor(Math.random() * 10)
+    };
+}
+
+function updateStatRow(teamStat)
+{
+    let statRow = document.getElementById("statsBody");
+    statRow.removeChild;
+    let newRow = document.createElement("tr");
+newRow.innerHTML = `
+        <td>${numMatch}</td>
+        <td>${points}</td>
+        <td>${numWin}</td>
+        <td>${numLoss}</td>
+        <td>${numDraw}</td>
+        <td>${numGoal}</td>
+        <td>${numRecu}</td>
+    `;
+
+}
+

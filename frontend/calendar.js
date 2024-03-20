7c6774837136fa9712f0d93f83136e6ca6346ce8
maasam514 <mohammadsam.karimi.1@ens.etsmtl.ca>
2024-03-19 23:50:57 -0400

    final push for tonight

--- frontend/calendar.js
+++ frontend/calendar.js
@@ -1,11 +1,11 @@
-document.getElementById("eventForm").addEventListener("submit", function(event) 
-{
+document.getElementById("eventForm").addEventListener("submit", function (event) {
     event.preventDefault();
-    
+
     let eventDate = document.getElementById("eventDate").value;
     let eventDescription = document.getElementById("eventDescription").value;
     let eventTime = document.getElementById("eventTime").value;
     let eventLocation = document.getElementById("eventLocation").value;
+    let eventLigue = document.getElementById("eventLigue").value;
 
     let newRow = document.createElement("tr");
     newRow.innerHTML = `
@@ -13,6 +13,7 @@ document.getElementById("eventForm").addEventListener("submit", function(event)
         <td>${eventDescription}</td>
         <td>${eventTime}</td>
         <td>${eventLocation}</td>
+        <td>${eventLigue}</td>
         <td><button class="deleteBtn">Delete</button></td>
     `;
 
@@ -23,7 +24,7 @@ document.getElementById("eventForm").addEventListener("submit", function(event)
 });
 
 // bouton pour effacer l'evenement
-document.getElementById("calendarBody").addEventListener("click", function(event) {
+document.getElementById("calendarBody").addEventListener("click", function (event) {
     if (event.target.classList.contains("deleteBtn")) {
         let row = event.target.closest("tr");
         row.remove();
